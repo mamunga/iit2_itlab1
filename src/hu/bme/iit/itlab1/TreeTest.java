6714474cacdd24021685a8902011b5a356ebe74c
phazek <phazek@bitbucket.org>
2014-05-04 15:01:21 +0200

    Main chunk and renderer 90%

--- src/hu/bme/iit/itlab1/TreeTest.java
+++ src/hu/bme/iit/itlab1/TreeTest.java
@@ -1,17 +1,44 @@
 package hu.bme.iit.itlab1;
 
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import javax.swing.JTree;
+
 /**
  * A főosztály. Felépíti a grafikus felületet.
  * 
  * @author mamunga
  * 
  */
-public class TreeTest {
+public class TreeTest implements ActionListener{
 
 	public static void main(String[] args) {
-		FileWrap wrapper = new FileWrap("resources/lol.txt");
-		System.out.println(wrapper.toString());
+		//setup frame
+		JFrame mainFrame = new JFrame("JTree");
+		mainFrame.setMinimumSize(new Dimension(180, 300));
+		
+		JTree fileTree = new JTree();
+		FileTreeModel model = new FileTreeModel();
+		FileTreeRenderer renderer = new FileTreeRenderer();
+		
+		//fileTree.setModel(model);
+		//fileTree.addActionListener(this);
+		
+		
+		JScrollPane jsp = new JScrollPane(fileTree);
+		mainFrame.add(jsp);
+		
+		mainFrame.setVisible(true);
+	}
 
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
