37ab6211e352c3ec5cf8cd40b7ed6a8240cf2107
phazek <phazek@bitbucket.org>
2014-05-04 20:51:58 +0200

    Combobox added

--- src/hu/bme/iit/itlab1/TreeTest.java
+++ src/hu/bme/iit/itlab1/TreeTest.java
@@ -1,9 +1,12 @@
 package hu.bme.iit.itlab1;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
@@ -17,16 +20,17 @@ import javax.swing.event.TreeSelectionListener;
  * @author mamunga
  * 
  */
-public class TreeTest implements TreeSelectionListener {
+public class TreeTest implements ActionListener {
 
 	static private JTree fileTree;
-	
+	static private JComboBox<FileWrap> comboBox;
 
 	public static void main(String[] args) {
 		// setup frame
 		JFrame mainFrame = new JFrame("JTree");
-		mainFrame.setMinimumSize(new Dimension(180, 300));
-
+		mainFrame.setMinimumSize(new Dimension(200, 350));
+		mainFrame.setLayout(new BorderLayout());
+		
 		fileTree = new JTree();
 		FileTreeModel model = new FileTreeModel();
 		FileTreeRenderer renderer = new FileTreeRenderer();
@@ -36,6 +40,9 @@ public class TreeTest implements TreeSelectionListener {
 		ToolTipManager.sharedInstance().registerComponent(fileTree);
 		fileTree.setCellRenderer(renderer);
 
+		comboBox = new JComboBox<FileWrap>();
+		mainFrame.add(comboBox, BorderLayout.SOUTH);
+		
 		JScrollPane jsp = new JScrollPane(fileTree);
 		mainFrame.add(jsp);
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -45,11 +52,9 @@ public class TreeTest implements TreeSelectionListener {
 
 
 	@Override
-	public void valueChanged(TreeSelectionEvent e) {
+	public void actionPerformed(ActionEvent arg0) {
 		// TODO Auto-generated method stub
-//		fileTree.get
-//		fileTree.expandPath(e.getPath());
+		
 	}
-
 	
 }
